77e498eec056bcc73c0f5f3e08522284d988b398
Sergio <patroclo404@gmail.com>
2017-12-12 22:58:05 -0600

    prod

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 let env = process.env.NODE_ENV || 'development';
 
-if( env === 'development' || env === 'test' || env === 'production' ){
+if( env === 'development' || env === 'test'){
   let config = require('./config.json');
   let envConfig = config[env];
 
